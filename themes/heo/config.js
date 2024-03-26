07fa69f9ce69cd1925d6ad9d1351c874559772f9
LeanZEN <131108101+XUTENGHAO@users.noreply.github.com>
2024-03-26 21:05:47 +0800

    Update config.js
    V1

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '执天之道', url: 'https://0zen.im' },
+    { title: '观天之行', url: 'https://0zen.im' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,16 +15,16 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '道通',
+  HEO_HERO_TITLE_2: '万法皆通',
+  HEO_HERO_TITLE_3: '〇ZEN.IM',
+  HEO_HERO_TITLE_4: '宇宙在乎手',
+  HEO_HERO_TITLE_5: '万化生乎身',
+  HEO_HERO_TITLE_LINK: 'https://0zen.im',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_2: { title: '大道本源', url: '/tag/热门文章' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
